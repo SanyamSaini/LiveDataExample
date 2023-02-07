d99ae84775ab536e490f08f0718c8021be016c6e
Sanyam Saini <sanyamsaini51@gmail.com>
2023-02-07 16:31:08 +0530

    Implemented Mutable and immutable Live Data.

--- app/src/main/java/com/sanyam/live/data/example/MainActivity.kt
+++ app/src/main/java/com/sanyam/live/data/example/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.sanyam.live.data.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainViewModel: MainViewModel
+    lateinit var tvFact: TextView
+    lateinit var btnUpdate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        tvFact = findViewById(R.id.tvFact)
+        btnUpdate = findViewById(R.id.btnUpdate)
+
+        mainViewModel.factsLiveData.observe(this, Observer {
+            tvFact.text = it
+        })
+
+        btnUpdate.setOnClickListener {
+            mainViewModel.updateLiveData()
+        }
     }
 }
